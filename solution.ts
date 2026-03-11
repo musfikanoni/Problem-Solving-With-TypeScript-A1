bc4de54d41adf18b3f504c53d1a170ab52660103
musfikanoni <musfikanoni@gmail.com>
2026-03-11 10:12:57 +0600

    Solved first problem

--- solution.ts
+++ solution.ts
@@ -1,5 +1,14 @@
-//problem 1
-const a = {
-    name: "oli",
-    age: 20
+
+const formatValue = (value: string | number | boolean): string | number | boolean => {
+    if (typeof value === "string") {
+        return value.toUpperCase();
+    }
+    if (typeof value === "number") {
+        return value * 10;
+    }
+    if (typeof value === "boolean") {
+        return !value;
+    }
+    return value;
 }
+
