52fcd19e2670d607db253446682e0d3305faed06
musfikanoni <musfikanoni@gmail.com>
2026-03-11 12:54:55 +0600

    solved problem 2 and 3

--- solution.ts
+++ solution.ts
@@ -3,12 +3,40 @@ const formatValue = (value: string | number | boolean): string | number | boolea
     if (typeof value === "string") {
         return value.toUpperCase();
     }
-    if (typeof value === "number") {
+    else if (typeof value === "number") {
         return value * 10;
     }
-    if (typeof value === "boolean") {
+    else {
         return !value;
     }
-    return value;
+}
+
+
+type Input = string | number[];
+
+const getLength = (input: Input): number => {
+    if(typeof input === "string") {
+        return input.length;
+    }
+    else if(Array.isArray(input)) {
+        return input.length;
+    }
+    return 0;
+}
+
+
+//problem 3 
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}`
+    }
 }
 
