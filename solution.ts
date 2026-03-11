9f15e621fd513ddb95ba1061443e72754c61cb76
musfikanoni <musfikanoni@gmail.com>
2026-03-12 01:45:41 +0600

    solved problem 4

--- solution.ts
+++ solution.ts
@@ -1,42 +1,55 @@
+//problem 1
+const formatValue = (
+  value: string | number | boolean,
+): string | number | boolean => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else {
+    return !value;
+  }
+};
 
-const formatValue = (value: string | number | boolean): string | number | boolean => {
-    if (typeof value === "string") {
-        return value.toUpperCase();
-    }
-    else if (typeof value === "number") {
-        return value * 10;
-    }
-    else {
-        return !value;
-    }
-}
 
-
+//Problem 2
 type Input = string | number[];
 
 const getLength = (input: Input): number => {
-    if(typeof input === "string") {
-        return input.length;
-    }
-    else if(Array.isArray(input)) {
-        return input.length;
-    }
-    return 0;
-}
+  if (typeof input === "string") {
+    return input.length;
+  } else if (Array.isArray(input)) {
+    return input.length;
+  }
+  return 0;
+};
 
 
-//problem 3 
+//problem 3
 class Person {
-    name: string;
-    age: number;
+  name: string;
+  age: number;
 
-    constructor(name: string, age: number){
-        this.name = name;
-        this.age = age;
-    }
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 
-    getDetails(): string {
-        return `Name: ${this.name}, Age: ${this.age}`
-    }
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  }
 }
+
+
+//problem 4
+
+type Item = {
+  title: string;
+  rating: number;
+};
+
+const filterByRating = (items: Item[]): Item[] => {
+  return items.filter((item) => item.rating >= 4);
+};
+
 
